50a68d8ff0d85976cf508c9da9e58dad841b9b01
Ethan Fisher <ethan.fisher32@gmail.com>
2023-07-27 17:05:40 -0600

    API weatrher

--- Weather/weather.js
+++ Weather/weather.js
@@ -1,7 +1,16 @@
-let x = numb() + " °C"
-document.getElementById("demo").innerHTML = x
-
-function numb() {
-    var random = Math.random() * (40 - (-40)) + -40
-    return Math.round(random * 10) / 10
-}
+$(document).ready(function(){
+    $("button").click(function(){
+        $.ajax({
+            url: 'http://api.weatherstack.com/current',
+            data: {
+            access_key: '061405da5207c73cb3ea74ce5d8f1679',
+            query: 'Calgary, Alberta'
+            },
+            dataType: 'json',
+            success: function(apiResponse) {
+                $("#div1").html(`Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`);
+                console.log();
+            }
+        });
+    });
+});
